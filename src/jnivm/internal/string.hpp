4608b293e42971f43e28a6758b26fe8fddc3b5a7
Christopher Homberger <christopher.homberger@web.de>
2021-02-08 17:30:50 +0100

    Implement Modified Utf8 to match spec

--- src/jnivm/internal/string.hpp
+++ src/jnivm/internal/string.hpp
@@ -1,46 +1,107 @@
 #include <jnivm/jnitypes.h>
 #include <jnivm/string.h>
-#include <locale>
-#include <climits>
 #include <sstream>
 
 namespace jnivm {
 
-    jstring NewString(JNIEnv *env, const jchar * str, jsize size) {
-        std::mbstate_t state{};
-        std::locale l;
-        auto& codecv = std::use_facet<std::codecvt<char16_t, char, std::mbstate_t>>(l);
-        std::stringstream ss;
-        char out[MB_LEN_MAX]{};
-        for (size_t i = 0; i < size; i++) {
-            const char16_t* from_next = 0;
-            char* to_next = 0;
-            auto r = codecv.out(state, (const char16_t*)&str[i], (const char16_t*)&str[i + 1], from_next, out, out + sizeof(out), to_next);
-        if(to_next != nullptr) {
-            ss.write(out, to_next - out);
+    static int UTFToJCharLength(const char * cur) {
+        if(!cur) {
+            throw std::runtime_error("UtfDataFormatError");
         }
+        if((*cur & 0b10000000) == 0) {
+            // Single ascii char
+            return 1;
+        } else if((*cur & 0b11100000) == 0b11000000) {
+            // unicode char pair
+            if((cur[1] & 0b11000000) != 0b10000000) {
+                throw std::runtime_error("UtfDataFormatError");
+            }
+            return 2;
+        } else if((*cur & 0b11110000) == 0b11100000) {
+            // unicode char 3 tuple
+            if((cur[1] & 0b11000000) != 0b10000000) {
+                throw std::runtime_error("UtfDataFormatError");
+            }
+            if((cur[2] & 0b11000000) != 0b10000000) {
+                throw std::runtime_error("UtfDataFormatError");
+            }
+            return 3;
+        } else {
+            throw std::runtime_error("UtfDataFormatError");
+        }
+    }
+
+    static jchar UTFToJChar(const char * cur, int& size) {
+        size = UTFToJCharLength(cur);
+        switch (size)
+        {
+        case 1:
+            return (jchar) *cur;
+        case 2:
+            return (jchar) (((*cur & 0x1F) << 6) | (cur[1] & 0x3F));
+        case 3:
+            return (jchar) (((*cur & 0x0F) << 12) | ((cur[1] & 0x3F) << 6) | (cur[2] & 0x3F));
+        default:
+            throw std::runtime_error("UtfDataFormatError");
+        }
+    }
+
+    static int JCharToUTFLength(jchar c) {
+        if(c == 0) {
+            return 2;
+        } else if((c & (0b10000000 - 1)) == c) {
+            return 1;
+        } else if((c & ((1 << 5 + 6) - 1)) == c) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    static int JCharToUTF(jchar c, char* cur, int len) {
+        int size = JCharToUTFLength(c);
+        if(size > len) {
+            throw std::runtime_error("End of String");
+        }
+        switch (size)
+        {
+        case 1:
+            cur[0] = (char) c;
+            break;
+        case 2:
+            cur[0] = (char) 0b11000000 | ((c >> 6) & 0x1F);
+            cur[1] = (char) 0b10000000 | (c & 0x3F);
+            break;
+        case 3:
+            cur[0] = (char) 0b11100000 | ((c >> 6) & 0x0F);
+            cur[1] = (char) 0b10000000 | ((c >> 12) & 0x3F);
+            cur[2] = (char) 0b10000000 | (c & 0x3F);
+            break;
+        }
+        return size;
+    }
+
+    jstring NewString(JNIEnv *env, const jchar * str, jsize size) {
+        std::stringstream ss;
+        char out[3];
+        for (size_t i = 0; i < size; i++) {
+            ss.write(out, JCharToUTF(str[i], out, sizeof(out)));
         }
         return (jstring)JNITypes<std::shared_ptr<String>>::ToJNIType((ENV*)env->functions->reserved0, std::make_shared<String>(ss.str()));
     };
     jsize GetStringLength(JNIEnv *env, jstring str) {
         if(str) {
-        mbstate_t state{};
-        std::locale l;
-        auto& codecv = std::use_facet<std::codecvt<char16_t, char, std::mbstate_t>>(l);
-        std::string * cstr = (String*)str;
-        size_t count = 0;
-        jsize length = 0;
-        jchar dummy;
-        auto cur = cstr->data(), end = cur + cstr->length();
-        
-        while(cur != end) {
-            const char* from_next = 0;
-            char16_t* to_next = 0;
-            auto r = codecv.in(state, (const char*)cur, (const char*)end, from_next, (char16_t*)&dummy, (char16_t*)&dummy + 1, to_next);
-            cur = (char*)from_next;
-            length++;
-        }
-        return length;
+            std::string * cstr = (String*)str;
+            size_t count = 0;
+            jsize length = 0;
+            jchar dummy;
+            auto cur = cstr->data(), end = cur + cstr->length();
+            
+            while(cur != end) {
+                cur += UTFToJCharLength(cur);
+                length++;
+            }
+            return length;
         } else {
             return 0;
         }
@@ -50,7 +111,7 @@ namespace jnivm {
         if(copy) {
             *copy = true;
         }
-        jsize length = env->GetStringLength(str);
+        jsize length = GetStringLength(env, str);
         // Allocate explicitly allocates string region
         jchar * jstr = new jchar[length];
         env->GetStringRegion(str, 0, length, jstr);
@@ -80,49 +141,37 @@ namespace jnivm {
     };
 
     void GetStringRegion(JNIEnv *, jstring str, jsize start, jsize length, jchar * buf) {
-        mbstate_t state{};
-        std::locale l;
-        auto& codecv = std::use_facet<std::codecvt<char16_t, char, std::mbstate_t>>(l);
         std::string * cstr = (String*)str;
         jchar dummy, * bend = buf + length;
         auto cur = cstr->data(), end = cur + cstr->length();
         while(start) {
-            const char* from_next = 0;
-            char16_t* to_next = 0;
-            auto r = codecv.in(state, (const char*)cur, (const char*)end, from_next, (char16_t*)&dummy, (char16_t*)&dummy + 1, to_next);
-            cur = (char*)from_next;
+            cur += UTFToJCharLength(cur);
             start--;
         }
         while(buf != bend) {
-            const char* from_next = 0;
-            char16_t* to_next = 0;
-            auto r = codecv.in(state, (const char*)cur, (const char*)end, from_next, (char16_t*)buf, (char16_t*)buf + 1, to_next);
-            cur = (char*)from_next;
+            int size;
+            *buf = UTFToJChar(cur, size);
+            cur += size;
             buf++;
         }
     };
+
     void GetStringUTFRegion(JNIEnv *, jstring str, jsize start, jsize len, char * buf) {
-        mbstate_t state{};
-        std::locale l;
-        auto& codecv = std::use_facet<std::codecvt<char16_t, char, std::mbstate_t>>(l);
         std::string * cstr = (String*)str;
         jchar dummy;
         char * bend = buf + len;
         auto cur = cstr->data(), end = cur + cstr->length();
         while(start) {
-            const char* from_next = 0;
-            char16_t* to_next = 0;
-            auto r = codecv.in(state, (const char*)cur, (const char*)end, from_next, (char16_t*)&dummy, (char16_t*)&dummy + 1, to_next);
-            cur = (char*)from_next;
+            cur += UTFToJCharLength(cur);
             start--;
         }
-        while(buf != bend) {
-            const char* from_next = 0;
-            char16_t* to_next = 0;
-            auto r = codecv.in(state, (const char*)cur, (const char*)end, from_next, (char16_t*)&dummy, (char16_t*)&dummy + 1, to_next);
-            memcpy(buf, cur, from_next - cur);
-            cur = (char*)from_next;
-            buf++;
+        while(len) {
+            int size = UTFToJCharLength(cur);
+            memcpy(buf, cur, size);
+            cur += size;
+            buf += size;
+            len--;
         }
+        *buf = '\0';
     };
 }
