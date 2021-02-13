90c89010dbd7d0b2c5adb3bfe1e3f5839c7e76c7
Christopher Homberger <christopher.homberger@web.de>
2021-02-13 10:49:54 +0100

    Add Extends wrapper class to provide inherience without using macros

--- include/jnivm/object.h
+++ include/jnivm/object.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <memory>
 #include <mutex>
+#include <vector>
 
 namespace jnivm {
     class Class;
@@ -14,6 +15,26 @@ namespace jnivm {
 
         virtual Class& getClass() {
             return *clazz.get();
+        }
+
+        static std::shared_ptr<Class> GetBaseClass(ENV* env) {
+            return nullptr;
+        }
+        static std::vector<std::shared_ptr<Class>> GetInterfaces(ENV* env) {
+            return {};
+        }
+    };
+
+    template<class Base, class... Interfaces> class Extends : public Base, public Interfaces... {
+    public:
+        static std::shared_ptr<Class> GetBaseClass(ENV* env) {
+            auto other = Base::GetBaseClass(env);
+            return env->vm->typecheck[typeid(Base)];
+        }
+        static std::vector<std::shared_ptr<Class>> GetInterfaces(ENV* env) {
+            auto interfaces = Base::GetInterfaces(env);
+            interfaces.insert(interfaces.end(), { env->vm->typecheck[typeid(Interfaces)]... });
+            return interfaces;
         }
     };
 }
