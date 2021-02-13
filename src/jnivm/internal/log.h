8ba54e92e1f7c56c3fef071d555033ade8fd12a6
Christopher Homberger <christopher.homberger@web.de>
2021-02-13 11:09:12 +0100

    fix logging macro

--- src/jnivm/internal/log.h
+++ src/jnivm/internal/log.h
@@ -3,5 +3,5 @@
 #include <log.h>
 #define LOG(...) Log::debug(__VA_ARGS__)
 #else
-#define LOG(tag, format, ...) printf("[" tag "]: " format "\n", __VA_ARGS__)
+#define LOG(tag, format, ...) printf("[" tag "]: " format "\n" , ##__VA_ARGS__)
 #endif
