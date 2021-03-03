92569555903076b4c64b758781f38f032e8ffcac
Christopher Homberger <christopher.homberger@web.de>
2021-03-03 15:07:35 +0100

    Remove cycle of findclass

--- include/jnivm/internal/findclass.h
+++ include/jnivm/internal/findclass.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <jni.h>
-#include <jnivm/class.h>
+#include <memory>
 
 namespace jnivm {
-
+    class Class;
+    class ENV;
     jclass InternalFindClass(JNIEnv *env, const char *name);
     std::shared_ptr<Class> InternalFindClass(ENV *env, const char *name);
     void Declare(JNIEnv *env, const char *signature);
