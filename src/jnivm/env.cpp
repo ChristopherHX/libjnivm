378170633afa74ea9c24c4f1c5afa8a9c00634cd
Christopher Homberger <christopher.homberger@web.de>
2021-03-05 20:04:08 +0100

    resolveReference: update to use JNITypes

--- src/jnivm/env.cpp
+++ src/jnivm/env.cpp
@@ -11,5 +11,5 @@ std::shared_ptr<Class> ENV::GetClass(const char * name) {
 
 //TODo use JNITYPES, casts here too.
 std::shared_ptr<Object> ENV::resolveReference(jobject obj) {
-    return obj ? ((Object*)obj)->shared_from_this() : nullptr;
+    return obj ? JNITypes<std::shared_ptr<Object>>::JNICast(this, obj) : nullptr;
 }
